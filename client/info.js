704986be3354d2fa758bcf1f9adac1f4505f3f87
sootn <encrypt.wave@gmail.com>
2015-04-10 01:01:52 +0200

    Start

--- client/info.js
+++ client/info.js
@@ -1,3 +1,19 @@
-var info = new Meteor.subscribe("info");
+metro = new Mongo.Collection("metro")
+bus = new Mongo.Collection("bus")
+tram = new Mongo.Collection("tram")
 
-console.log(info.find().fetch())
+Meteor.subscribe("metro")
+Meteor.subscribe("bus")
+Meteor.subscribe("tram")
+
+Router.route('/metro', function () {
+  this.render('metro');
+})
+
+Router.route('/bus', function () {
+  this.render('bus');
+})
+
+Router.route('/tram', function () {
+  this.render('tram');
+})
--- server/info.js
+++ server/info.js
@@ -3,16 +3,13 @@ var bus = new Mongo.Collection("bus")
 var tram = new Mongo.Collection("tram")
 
 
-var url = "http://api.sl.se/api2/realtimedepartures.json?key=&siteid=9204&timewindow=60"
+var url = "http://api.sl.se/api2/realtimedepartures.json?key=01faef2ce14d480db2f0b5e52bed7fbc&siteid=9204&timewindow=60"
 Meteor.http.get(url, function(a, data) {
-	metro.remove({})
-	tram.remove({})
-	bus.remove({})
-	var rd = JSON.parse(data.content).ResponseData
-	metro.insert(rd.Metros)
-	bus.insert(rd.Buses)
-	tram.insert(rd.Trams)
-	console.log(metro.find({}).fetch())
-	console.log(tram.find({}).fetch())
-	console.log(bus.find({}).fetch())
+	[metro, tram, bus].forEach(function(collection) {
+		collection.remove({})
+		var rd = JSON.parse(data.content).ResponseData
+		rd.Metros.forEach(function(m) {
+			collection.insert(m)
+		})
+	})
 })
