ee0edd055ca7345bed149d9ecdd9698989c8d852
Jesper Simonsson <simonsson.jesper@gmail.com>
2015-04-10 16:16:17 +0200

    data

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.sl = {};
+config.sl.storningtoken = "key";
+config.sl.platstoken = "key";
+config.sl.realtidtoken = "key";
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,10 +1,55 @@
+var config = require('./config');
 var app = require('express')();
 var http = require('http').Server(app);
+var xhttp = require('http');
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
+
+//----------- SL STORNING
+var realtidoptions = {
+	host: 'api.sl.se',
+	path: '/api2/realtimedepartures.json?key=' + config.sl.realtidtoken + '&siteid=9204&timewindow=60',
+	method: 'GET'
+};
+
+var getstorning = function(callback) {
+	var req = xhttp.request(realtidoptions, function(res) {
+		res.setEncoding('utf-8');
+
+		var responseString = '';
+		res.on('data', function(data) {
+			responseString += data;
+		});
+
+		res.on('end', function() {
+			console.log(responseString);
+			var responseObject = JSON.parse(responseString);
+			callback(responseObject);
+		});
+
+		res.on('error', function(e) {
+			console.log(e);
+		});
+	});
+	req.end();
+};
+
+getstorning(function(data) {
+	console.log(data);
+});
+
+//----------- IO
+io.on('connection', function(socket) {
+	console.log('new connection');
+	socket.on('disconnect', function() {
+		console.log('connection closed');
+	});
+});
+
 http.listen(3000, function(){
 	console.log('listening on *:3000');
-})
+});
