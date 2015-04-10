469682b5b6f92bc514d198b78bb73daba55aa2f2
sootn <encrypt.wave@gmail.com>
2015-04-10 02:11:09 +0200

    Bla

--- server/info.js
+++ server/info.js
@@ -2,14 +2,18 @@ var metro = new Mongo.Collection("metro")
 var bus = new Mongo.Collection("bus")
 var tram = new Mongo.Collection("tram")
 
-
+var MINUTES_BETWEEN_REFRESH = 15
 var url = "http://api.sl.se/api2/realtimedepartures.json?key=01faef2ce14d480db2f0b5e52bed7fbc&siteid=9204&timewindow=60"
-Meteor.http.get(url, function(a, data) {
-	[metro, tram, bus].forEach(function(collection) {
-		collection.remove({})
-		var rd = JSON.parse(data.content).ResponseData
-		rd.Metros.forEach(function(m) {
-			collection.insert(m)
+function refresh() {
+	Meteor.http.get(url, function(a, data) {
+		[metro, tram, bus].forEach(function(collection) {
+			var rd = JSON.parse(data.content).ResponseData
+			collection.remove({})
+			rd.Metros.reverse().forEach(function(m) {
+				collection.insert(m)
+			})
 		})
 	})
-})
+}
+setInterval(refresh, 1000 * 60 * MINUTES_BETWEEN_REFRESH)
+refresh()
