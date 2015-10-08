df3b3535f0dc45119543ce4e3efe04d47c74f52a
Jesper Simonsson <simonsson.jesper@gmail.com>
2015-10-08 16:52:30 +0200

    moved tokens to env

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.sl = {};
+config.sl.storningtoken = process.env.STORNINGTOKEN; //only stored in env
+config.sl.realtidtoken = process.env.REALTIDTOKEN; //only stored in env
+config.port = process.env.PORT || 5000;
+config.refreshrate = {};
+config.refreshrate.realtid = process.env.REALREFRESH || 60 ; //Number of seconds between refresh
+config.refreshrate.storning = process.env.STORREFRESH ||300; //Number of seconds between refresh
+config.siteid = process.env.SITEID || 9204; //Tekniska högskolan siteid
+
+module.exports = config;
