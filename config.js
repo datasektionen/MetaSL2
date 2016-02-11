8175f08937cb06f90b63e971f419413076a80cbd
Jesper Simonsson <simonsson.jesper@gmail.com>
2016-02-11 17:01:39 +0100

    updated refreshtime to be 30s

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ config.sl.storningtoken = process.env.STORNINGTOKEN; //only stored in env
 config.sl.realtidtoken = process.env.REALTIDTOKEN; //only stored in env
 config.port = process.env.PORT || 5000;
 config.refreshrate = {};
-config.refreshrate.realtid = process.env.REALREFRESH || 60 ; //Number of seconds between refresh
+config.refreshrate.realtid = process.env.REALREFRESH || 30 ; //Number of seconds between refresh
 config.refreshrate.storning = process.env.STORREFRESH ||300; //Number of seconds between refresh
 config.siteid = process.env.SITEID || 9204; //Tekniska högskolan siteid
 
