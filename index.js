9950a75c27fa6f1b687bb4beb0e07d295bdf0be1
Jesper Simonsson <jsimo@kth.se>
2017-10-12 13:25:25 +0200

    Updated to support latest nodejs version Added request to simplify handling of http calls and defined properly in package.json support for what node version and more

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var server= require('http').Server(app);
 var http = require('http');
 var io = require('socket.io')(server);
 var path = require('path');
+var request = require('request');
 require('./routes')(app);
 
 //----------- public folder
@@ -12,47 +13,36 @@ app.use('/public', express.static(path.join(__dirname + '/public')));
 
 //----------- SL OPTIONS
 var sloptionsrealtid = {
-	host: 'api.sl.se',
-	path: '/api2/realtimedeparturesV4.json?key=' + config.sl.realtidtoken + '&siteid=' + config.siteid + '&timewindow=60',
-	method: 'GET'
+    uri: 'http://api.sl.se/api2/realtimedeparturesV4.json?key=' + config.sl.realtidtoken + '&siteid=' + config.siteid + '&timewindow=60',
+    method: 'GET'
 };
 
 //----------- stats
 var stats = {
-	latesttime:"",
-	requests:0,
-	nrofclients:0,
+    latesttime:"",
+    requests:0,
+    nrofclients:0,
 };
 
 //----------- get stuff
 var getstuff = function(options, callback) {
-	stats.requests++;
-	var req = http.request(options, function(res) {
-		res.setEncoding('utf-8');
-
-		var responseString = '';
-		res.on('data', function(data) {
-			responseString += data;
-		});
-
-		res.on('end', function() {
-			if (responseString) {
-				try {
-					var responseObject = JSON.parse(responseString);
-					callback(responseObject);
-				} catch(e) { 
-					console.log("response:" + responseString);
-					console.log("error: " + e);
-					console.log(e.stack);
-				}
-			}			
-		});
-
-		res.on('error', function(e) {
-			console.log(e);
-		});
-	});
-	req.end();
+    stats.requests++;
+    request(options, function (error, response, body) {
+        if(error) console.log('error:', error); 
+        //Only do things if the response is the expected json response
+        if (body && response.headers['content-type'].includes('application/json')) {
+            try {
+                var responseObject = JSON.parse(body);
+            } catch(e) {
+                console.log("response:" + body);
+                console.log("error: " + e);
+                console.log(e.stack);
+            }
+            callback(responseObject);
+        } else {
+            console.log("Error: not json data");
+        }
+    });
 };
 
 //-----------
@@ -61,43 +51,46 @@ var sltraffic = {};
 
 //----------- IO
 io.on('connection', function(socket) {
-	stats.nrofclients = io.engine.clientsCount;	
-	if (sldata.ResponseData) { //On upstart there will be no data here.
-		socket.emit('slmetro', sldata.ResponseData.Metros);
-		socket.emit('slbus', sldata.ResponseData.Buses);
-		socket.emit('sltram', sldata.ResponseData.Trams);
-		socket.emit('stats', stats);
-		//socket.emit('sltrafficinfo', sltraffic);
-	};
-	socket.on('disconnect', function() {
-		stats.nrofclients = io.engine.clientsCount;
-	});
+    stats.nrofclients = io.engine.clientsCount; 
+    if (sldata.ResponseData) { //On upstart there will be no data here.
+        socket.emit('slmetro', sldata.ResponseData.Metros);
+        socket.emit('slbus', sldata.ResponseData.Buses);
+        socket.emit('sltram', sldata.ResponseData.Trams);
+        socket.emit('stats', stats);
+        //socket.emit('sltrafficinfo', sltraffic);
+    };
+    socket.on('disconnect', function() {
+        stats.nrofclients = io.engine.clientsCount;
+    });
 });
 
 //----------- Realtid
 var getRealtime = function(){
-	getstuff(sloptionsrealtid, function(data) {
-		sldata = data;
-		//Update the latest succesfull requests time.
-		if (sldata.ResponseData && sldata.ResponseData.LatestUpdate) {
-			stats.latesttime = sldata.ResponseData.LatestUpdate;
-		}
-		io.emit('slmetro', sldata.ResponseData.Metros);
-		io.emit('slbus', sldata.ResponseData.Buses);
-		io.emit('sltram', sldata.ResponseData.Trams);
-		io.emit('stats', stats);
-	});
+    getstuff(sloptionsrealtid, function(data) {
+        //Update the latest succesfull requests time.
+        if (data.ResponseData && data.ResponseData.LatestUpdate
+            && !data.ResponseData.LatestUpdate.includes('1970')) {
+            stats.latesttime = data.ResponseData.LatestUpdate;
+            sldata = data;
+            io.emit('slmetro', sldata.ResponseData.Metros);
+            io.emit('slbus', sldata.ResponseData.Buses);
+            io.emit('sltram', sldata.ResponseData.Trams);
+            io.emit('stats', stats);
+        } else {
+            console.log("some error with the data");
+        }
+    });
 };
 
 setInterval(getRealtime, 1000 * config.refreshrate.realtid); //Refreshrate is in seconds.
 getRealtime();
 
 process.on('uncaughtException', function globalErrorCatch(error, p) {
-	console.error(error);
-	console.error(error.stack);
+    console.error(error);
+    console.error(error.stack);
 });
 
 //---------
 server.listen(config.port, function(){
-	console.log('listening on *:' + config.port);
+    console.log('listening on *:' + config.port);
 });
